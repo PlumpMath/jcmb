e529f3c70738a63b009980ba77eb81cf354d8a2f
vixus0 <vixus0@gmail.com>
2013-12-31 04:00:45 +0000

    added newjcmb.py for dynamic loading

--- src/newjcmb.py
+++ src/newjcmb.py
@@ -10,46 +10,51 @@ from Player import *
 
 class EscapeFromJCMB(object,DirectObject):
 
-	def __init__(self):
-		self.init_window()
-		self.init_bullet_world()
-		self.load_game_world()
-		self.load_player()
-		self.start_physics()
-	
-	def init_window(self):
-		# Hide the mouse cursor and set a title
-		props = WindowProperties()
-		props.setCursorHidden(True)
-		props.setTitle('EscapeFromJCMB')
-		base.win.requestProperties(props)
-	
-		# Allow us to exit the game at any time with Esc
-		self.accept('escape', sys.exit)
-	
-	def init_bullet_world(self):
-		self.bullet_world = BulletWorld()
-		self.bullet_world.setGravity(Vec3(0, 0, -9.81))		
+  def __init__(self):
+    self.init_window()
+    self.init_bullet_world()
+    self.load_game_world()
+    self.load_player()
+    self.start_physics()
+  
+  def init_window(self):
+    # Hide the mouse cursor and set a title
+    props = WindowProperties()
+    props.setCursorHidden(True)
+    props.setTitle('EscapeFromJCMB')
+    base.win.requestProperties(props)
+  
+    # Allow us to exit the game at any time with Esc
+    self.accept('escape', sys.exit)
+  
+  def init_bullet_world(self):
+    self.bullet_world = BulletWorld()
+    self.bullet_world.setGravity(Vec3(0, 0, -9.81))   
 
-	def load_game_world(self):
-		stairwell = Room('../data/mod/jcmbstairs.egg')
-		stairwell.add_all_visibles_and_collidables(render, self.bullet_world)
+  def load_game_world(self):
+    stairwell = Room('../data/mod/jcmbstairs.egg')
+    stairwell.add_all_visibles_and_collidables(render, self.bullet_world)
 
-	def load_player(self):
-		self.player = Player(base.camera, base.camLens, self.bullet_world, render, taskMgr)
-#		self.player.add_all_visibles_and_collidables(render, self.bullet_world)
+  def load_player(self):
+    self.player = Player(base.camera, base.camLens, self.bullet_world, render, taskMgr)
+#   self.player.add_all_visibles_and_collidables(render, self.bullet_world)
 
-	def update_physics(self, task):
-		dt = globalClock.getDt()
-		self.bullet_world.doPhysics(dt)
-		return task.cont
+  def update_physics(self, task):
+    dt = globalClock.getDt()
+    self.bullet_world.doPhysics(dt)
+    return task.cont
 
-	def start_physics(self):
-		taskMgr.add(self.update_physics, 'update_physics')
+  def start_physics(self):
+    taskMgr.add(self.update_physics, 'update_physics')
 
 
 # Get this show on the road
 base.setFrameRateMeter(True)
 EscapeFromJCMB()
 render.setShaderAuto()
+bgmusic = base.loader.loadSfx("../data/snd/Bent_and_Broken.mp3")
+bgmusic.play()
+bgmusic.setVolume(1.0)
+if bgmusic.status() == bgmusic.PLAYING:
+  print "WOO"
 run()
